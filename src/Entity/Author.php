202dfeb917ee6388cfcf0d63805b778d2f57179b
Ippei Sumida <ippey.s@gmail.com>
2022-07-03 21:35:10 +0900

    feat: upgrade api-platform 3.0(2.7)

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,68 +2,72 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    normalizationContext: ['groups' => ['author']]
+)]
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
-#[ApiResource]
 class Author
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['author'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['author'])]
     private $name;
 
     #[ORM\OneToMany(mappedBy: 'author', targetEntity: Book::class)]
+    #[Groups(['author'])]
     private $books;
 
     public function __construct()
     {
         $this->books = new ArrayCollection();
     }
-
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
-
-    public function getName(): ?string
+    public function getName() : ?string
     {
         return $this->name;
     }
-
-    public function setName(string $name): self
+    public function setName(string $name) : self
     {
         $this->name = $name;
-
         return $this;
     }
-
     /**
      * @return Collection<int, Book>
      */
-    public function getBooks(): Collection
+    public function getBooks() : Collection
     {
         return $this->books;
     }
-
-    public function addBook(Book $book): self
+    public function addBook(Book $book) : self
     {
         if (!$this->books->contains($book)) {
             $this->books[] = $book;
             $book->setAuthor($this);
         }
-
         return $this;
     }
-
-    public function removeBook(Book $book): self
+    public function removeBook(Book $book) : self
     {
         if ($this->books->removeElement($book)) {
             // set the owning side to null (unless already changed)
@@ -71,7 +75,6 @@ class Author
                 $book->setAuthor(null);
             }
         }
-
         return $this;
     }
 }
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,82 +2,86 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Link;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
-#[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource]
+#[ApiResource(
+    uriTemplate: '/authors/{authorId}/books',
+    operations: [new GetCollection()],
+    uriVariables: ['authorId' => new Link(toProperty: 'author', fromClass: Author::class)]
+)]
+#[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['author'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['author'])]
     private $title;
 
     #[ORM\Column(type: 'boolean')]
+    #[Groups(['author'])]
     private $isPublished;
 
     #[ORM\Column(type: 'integer')]
+    #[Groups(['author'])]
     private $price;
 
     #[ORM\ManyToOne(targetEntity: Author::class, inversedBy: 'books')]
     #[ORM\JoinColumn(nullable: false)]
     private $author;
-
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
-
-    public function getTitle(): ?string
+    public function getTitle() : ?string
     {
         return $this->title;
     }
-
-    public function setTitle(string $title): self
+    public function setTitle(string $title) : self
     {
         $this->title = $title;
-
         return $this;
     }
-
-    public function isIsPublished(): ?bool
+    public function isIsPublished() : ?bool
     {
         return $this->isPublished;
     }
-
-    public function setIsPublished(bool $isPublished): self
+    public function setIsPublished(bool $isPublished) : self
     {
         $this->isPublished = $isPublished;
-
         return $this;
     }
-
-    public function getPrice(): ?int
+    public function getPrice() : ?int
     {
         return $this->price;
     }
-
-    public function setPrice(int $price): self
+    public function setPrice(int $price) : self
     {
         $this->price = $price;
-
         return $this;
     }
-
-    public function getAuthor(): ?Author
+    public function getAuthor() : ?Author
     {
         return $this->author;
     }
-
-    public function setAuthor(?Author $author): self
+    public function setAuthor(?Author $author) : self
     {
         $this->author = $author;
-
         return $this;
     }
 }
